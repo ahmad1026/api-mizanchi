c9d5401010afed7cc406ea1ce40b6f343d2000b6
ahmad1026 <a.mohammadi1026@gmail.com>
2023-09-05 19:46:58 +0330

    second commit

---
+++ app.js
@@ -0,0 +1,19 @@
+console.log("hello demo");
+
+const express = require('express')
+
+const app = express()
+
+
+app.get('/' , (req , resp)=>{
+    return resp.json({
+        name:"ahmad"
+    })
+})
+
+
+
+app.listen(3000 , ()=>{
+
+    console.log("app is running");
+})
